21e39b33bfe320338b2a306536795be225fe4d1d
Yan Azevedo <yanazevedoif@outlook.com>
2022-06-25 15:45:24 -0300

    SH1sync

---
+++ ConnectExchangeOnline.ps1
@@ -0,0 +1,13 @@
+Set-ExecutionPolicy Unrestricted -Force
+
+Set-ExecutionPolicy RemoteSigned
+
+Enable-PSRemoting
+
+Install-Module -Name ExchangeOnlineManagement
+
+Import-Module ExchangeOnlineManagement
+
+Connect-ExchangeOnline
+
+Connect-IPPSSession
